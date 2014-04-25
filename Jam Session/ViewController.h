1b787552797f18b55a1d38af8e143acc35cafb12
Matt Silas <matthewj.silas@gmail.com>
2014-04-25 12:59:14 -0600

    Implemented basic central delegate methods. Added peripheral and peripheralManager protocols. TO-DO: begin refactoring CentralManager and PeripheralManager as own classes instead of using their delegate methods all in the View Controller.

--- Jam Session/ViewController.h
+++ Jam Session/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController <CBPeripheralManagerDelegate, CBPeripheralDelegate,CBCentralManagerDelegate>
+
 
 @end
--- Jam Session/ViewController.m
+++ Jam Session/ViewController.m
@@ -9,15 +9,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    CBMutableService *broadcastService;
+    CBMutableDescriptor *broadcastDescriptor;
+    
+    CBService *receivedService;
+    CBDescriptor *receivedDesriptor;
+    
+    NSMutableData *data;
+    CBPeripheralManager *peripheralManager;
+    CBCentralManager *centralManager;
+    __strong IBOutlet UISegmentedControl *broadcastReceiveControl;
+}
 @end
 
-@implementation ViewController
+
+@implementation ViewController 
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    CGRect frame = broadcastReceiveControl.frame;
+    [broadcastReceiveControl setFrame:CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 800) ];
+    
+    centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil];
+    
+    data = [[NSMutableData alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +45,76 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void) peripheralManagerDidUpdateState:(CBPeripheralManager *) peripheral
+{
+    switch (peripheral.state) {
+        case CBPeripheralManagerStatePoweredOn:
+            [self setupService];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+-(void) centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    if (central.state != CBCentralManagerStatePoweredOn)
+    {
+        return;
+    }
+}
+
+-(void) centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    [centralManager stopScan];
+    [centralManager connectPeripheral:peripheral options:nil];
+}
+
+- (void) centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    peripheral.delegate = self;
+    //TO-DO create NSArray of services matching the broadcasting peripheral
+    [peripheral discoverServices:nil];
+    
+}
+
+-(void) peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    
+}
+
+-(void) setupService
+{
+    
+}
+
+-(void) startScan
+{
+    //TO-DO create an array of CBUUIDs that match what the peripheral advertises
+    [centralManager scanForPeripheralsWithServices:nil options:nil];
+}
+
+-(void) startBroadcast
+{
+    
+}
+
+- (IBAction)broadcastReceiveControlValueDidChange:(id)sender
+{
+    UISegmentedControl *segmentedControl = (UISegmentedControl*) sender;
+    bool receiveMode = segmentedControl.selectedSegmentIndex;
+    if(receiveMode)
+    {
+        [self startScan];
+    }
+    else
+    {
+        
+    }
+}
+
+
 
 @end
